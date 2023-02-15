1898322518947bb4a147e8bb77d2bdf2fa23c0f6
Jesus Pineda <jesuspineda18@outlook.es>
2023-02-15 16:27:11 -0500

    get affiliate

--- routes/affiliate.js
+++ routes/affiliate.js
@@ -4,5 +4,5 @@ const AffiliateController = require('../controllers/affiliate-controller')
 
 router.post('/', AffiliateController.create)
 router.post('/auth', AffiliateController.login)
-
+router.get('/:id', AffiliateController.getAffiliateById)
 module.exports = router
