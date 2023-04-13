e99db31d5c4368b3de7e5793c7f2279b0d48d010
Jesus Pineda <jesuspineda18@outlook.es>
2023-04-13 14:52:27 -0500

    add change read notification

--- controllers/notification-controller.js
+++ controllers/notification-controller.js
@@ -2,7 +2,7 @@ const { request, response } = require("express");
 const NotificationModel = require('../models/notification')
 
 const NotificationController = {
-    getNotificationsById : async (req, request, res = response) => {
+    getNotificationsById : async (req = request, res = response) => {
         try {
             const {id} = req.params
 
@@ -22,7 +22,35 @@ const NotificationController = {
                 error: false,
                 data: notifications
             })
-            
+
+        } catch (error) {
+            return res.status(500).json({
+                msg: 'Error en el servidor ' + error,
+                error: true
+            })
+        }
+    },
+
+    changeRead: async  (req = request, res = response) => {
+        try {
+            const {id} = req.params
+
+            const notification = await NotificationModel.findByIdAndUpdate(id,{
+                status: 'READ'
+            })
+
+            if(notification == null){
+                return res.status(200).json({
+                    msg: 'No se pudo actualizar la notificacion',
+                    error: true
+                })
+            }
+
+            return res.status(200).json({
+                msg: 'Notificacion actualizada',
+                error: true,
+                data: notification
+            })
         } catch (error) {
             return res.status(500).json({
                 msg: 'Error en el servidor ' + error,
--- routes/notification.js
+++ routes/notification.js
@@ -3,5 +3,6 @@ var router = express.Router()
 const NotificationController = require('../controllers/notification-controller')
 
 router.get('/:id', NotificationController.getNotificationsById)
+router.put('/:id', NotificationController.changeRead)
 
 module.exports = router
